e711d4a4d2416e7827fce726186ac86683a2bb5c
Keenan Aldridge <kmantan@gmail.com>
2022-07-19 10:59:28 -1000

    Added background and basic layout for search/entry

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import { hot } from 'react-hot-loader/root';
 
 class App extends React.Component {
   render() {
-    const { name } = this.props;
+    const { name, dexNum } = this.props;
     return (
       <>
         <h1>
-          Hello {name}
+          Welcome to {name}'s Pokedex
         </h1>
+        <div className="entry">
+          <img src={"https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + dexNum + ".png"} width="240"></img>
+          <p>Grass   Poison</p>
+          <p>Height: 'height.goes.here'</p>
+          <p>Weight: 'weight.goes.here'</p>
+        </div>
+        <form action='http://localhost:3000/' type='POST'>
+        <input type="text" placeholder="Enter Pokemon Name"></input>
+        <button type="submit">Find Pokemon</button>
+        </form>
       </>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-
 var root = document.getElementById("app");
-ReactDOM.render(<App name="Keenan" />, root);
+ReactDOM.render(<App name="Keenan" dexNum="001"/>, root);

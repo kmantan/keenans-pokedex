41b0eb890f79190386cf08ab8932f164e01517fb
Keenan Aldridge <kmantan@gmail.com>
2022-07-21 16:54:56 -1000

    Implemeted MVP

--- server/index.js
+++ server/index.js
@@ -2,22 +2,43 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const pokeDb = require('../database/db')
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+// app.use(bodyParser())
 
-app.get('/', (req, res) => {
-  console.log(req);
-  let requestedPokemon = req.body.name.toLowerCase();
+app.post('/', (req, res) => {
 
-  axios.get('https://pokeapi.co/api/v2/pokemon/' + requestedPokemon)
-  .then((data) => {
-    console.log('Returned Data: ', data);
-    res.send('allTheInfo.JSON')
+  let pokemonName = req.body.name;
+
+  pokeDb.lookForPokemon(pokemonName)
+  .then((resultFromDb) => {
+    if(resultFromDb.length === 0) {
+      axios.get('https://pokeapi.co/api/v2/pokemon/' + pokemonName)
+      .then((response) => {
+        pokeDb.savePokemon(response.data);
+        res.json(response.data);
+      })
+      .catch((err) => {
+        console.log('Error: ', err);
+      })
+    } else {
+      res.json(resultFromDb[0])
+    }
   })
-  .catch((err) => {
-    console.log('Error: ', err);
-  })
+  // res.json({hello: 'hello'})
+
+
+
+
 });
+
+app.get('/', (req,res) => {
+  console.log('req.body');
+})
 
 app.listen(3000, () => {
   console.log('Now listening to port 3000.....');
